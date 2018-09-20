ff1cb62df85db741a5df8a94f32a75215cf8a442
Els <Emathivannan@spartaglobal.com>
2018-09-20 11:56:25 +0100

    Changes

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -13,12 +13,16 @@ sudo apt-get install nginx -y
 sudo apt-get install git -y
 
 # install nodejs
-sudo apt-get install python-software-properties
+sudo apt-get install -y python-software-properties
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
 # install pm2
-sudo npm install pm2 -g
+sudo npm install -y pm2 -g
+
+# remove the default folders
+sudo rm -rf /etc/nginx/sites-enabled
+sudo rm -rf /etc/nginx/sites-available
 
 # set up nginx reverse proxy
 sudo ln -s /home/ubuntu/environment/nodeapp.conf /etc/nginx/conf.d/nodeapp.conf
